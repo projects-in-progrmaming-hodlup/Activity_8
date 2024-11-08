4ede28b21c4f8cec79660dc973926499d189e55b
Molly Zhang <yz9753@nyu.edu>
2024-11-07 19:10:03 -0500

    Update Dropdown.js

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,15 +1,15 @@
-// src/components/Dropdown.js
 import React from 'react';
-import './Dropdown.css';
 
 const Dropdown = ({ label, options, onSelect, selectedValue }) => {
   return (
     <div>
       <label>{label}</label>
-      <select value={selectedValue} onChange={(e) => onSelect(e.target.value)}>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
+      <select 
+        value={selectedValue} 
+        onChange={(e) => onSelect(e.target.value)}>
+        {options.map((option, index) => (
+          <option key={index} value={option.value || option}>
+            {option.label || option} {/* Show the label if it's an object, or the value if it's not */}
           </option>
         ))}
       </select>
